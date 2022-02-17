514995bfd84375a6629144f08261865cfa722b56
[플랫폼/이성민] <stiner01@nm-fc.com>
2022-02-17 11:23:19 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace MyToastNofify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 4)
+                return;
+
+            string title = string.Empty;
+            string message = string.Empty;
+
+            for (int i = 0, max = args.Length; i < max; i++)
+            {
+                if (args[i].Equals("--title"))
+                {
+                    title = args[i + 1];
+                }
+                if (args[i].Equals("--message"))
+                {
+                    message = args[i + 1];
+                }
+            }
+
+            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(message))
+                return;
+
+            new MyNotification().Run(title, message);
+            Thread.Sleep(1);
+        }
+    }
+
+    public class MyNotification
+    {
+        public void Run(string title, string message)
+        {
+            var toast = new ToastContentBuilder();
+            toast.AddText(title);
+            toast.AddText(message);
+            toast.Show();
+        }
+    }
+}
